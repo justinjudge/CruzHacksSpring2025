bab66c36b69c234294da8489c226b744698e3338
Eric Anderson <ericcand@outlook.com>
2025-04-13 02:42:38 -0700

    fixed bugs

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -7,11 +7,10 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI
 const options: MongoClientOptions = {}
 
-// Correctly type the global declaration
-declare global {
-  // eslint-disable-next-line no-var
-  var _mongoClientPromise: Promise<MongoClient> | undefined
-}
+// Global is used here to maintain a cached connection across hot reloads
+// in development. This prevents connections growing exponentially
+// during API Route usage.
+let _mongoClientPromise: Promise<MongoClient> | undefined
 
 let client: MongoClient
 let clientPromise: Promise<MongoClient>
@@ -19,11 +18,11 @@ let clientPromise: Promise<MongoClient>
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
+  if (!_mongoClientPromise) {
     client = new MongoClient(uri, options)
-    global._mongoClientPromise = client.connect()
+    _mongoClientPromise = client.connect()
   }
-  clientPromise = global._mongoClientPromise!
+  clientPromise = _mongoClientPromise!
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options)
